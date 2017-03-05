668a47ee57d4ea74864d6a8a33b6ca3e96c18a18
raavr <rafal.r26@gmail.com>
2017-03-05 20:49:50 +0100

    added max-width container

--- src/app/app.component.js
+++ src/app/app.component.js
@@ -1,8 +1,10 @@
 export const AppComponent = {
     template: `
-        <navbar></navbar>
-        <ui-view></ui-view>
-        <app-footer></app-footer>
+        <div class="app-wrapper">
+            <navbar></navbar>
+            <ui-view></ui-view>
+            <app-footer></app-footer>
+        </div>
     `
 }
 
